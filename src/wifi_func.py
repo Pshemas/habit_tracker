8cdc9f8fc8596f0beb00afbba26026d94886c1d2
Przemek Jeske <jeskenis@gmail.com>
2026-03-23 21:33:17 +0100

    wifi connect tweak

--- src/wifi_func.py
+++ src/wifi_func.py
@@ -10,6 +10,7 @@ def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wifi_creds = []
+    sleep(1)
 
     with open(".wifi", "r") as f:
         for line in f:
@@ -18,13 +19,12 @@ def connect_wifi():
     for i in range(5):
         print("connection attempt:", i + 1)
         wlan.connect(wifi_creds[0], wifi_creds[1])
+        sleep(3)
 
         if wlan.status() == 3:
             break
         elif wlan.status() == 1:
             print("connecting")
-
-        sleep(3)
 
     if wlan.status() != 3:
         raise NoWifiError
